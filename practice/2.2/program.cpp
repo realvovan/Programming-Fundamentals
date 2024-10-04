3c4e0a00c08e1e444662db81b9fccde17aa0c978
vovan <vovagor1234321@gmail.com>
2024-10-04 12:13:18 +0300

    Completed practice 2.2

--- practice/2.2/program.cpp
+++ practice/2.2/program.cpp
@@ -10,10 +10,20 @@
 #include "stdlib.h"
 #include "time.h"
 #include "iomanip"
+#include "algorithm"
 
 using std::vector;
 using std::cout;
 using std::setw;
+
+//populates the given vector with random integers in range [1,19] and prints it in the output
+void populateVector(vector<int> &sequence) {
+    for(int i = 0; i < sequence.size(); i++) {
+        sequence[i] = rand() % 20;
+        cout << setw(2) << sequence[i] << " ";
+    }
+    cout << "\n";
+}
 
 void task1() {
     // Задано множину послідовностей значень A[m,n].
@@ -48,8 +58,33 @@ void task1() {
     }
 }
 
+void task2() {
+    // Задано дві послідовності значень A[n] і B[m]. Замінити в A[n] входження значень із B[m] на добуток елементів
+    // підпослідовності B[m]
+    cout << "====================\nTask 2\n";
+    const int VECTOR_SIZE = 5;
+    int product = 1;
+    vector<int> A(VECTOR_SIZE),B(VECTOR_SIZE);
+    cout << "Initial vector A: ";
+    populateVector(A);
+    cout << "Initial vector B: ";
+    populateVector(B);
+    for(int i : B) product *= i;
+    cout << "The product of elements of vector B is " << product << "\n";
+    for(int i = 0; i < VECTOR_SIZE; i++) {
+        //check if vector B contains elemnt A[i]
+        if(std::find(B.begin(),B.end(),A[i]) != B.end()) A[i] = product;
+    }
+    cout << "Updated vector A: ";
+    for(int i : A) {
+        cout << setw(2) << i << " ";
+    }
+    cout << "\n";
+}
+
 int main() {
     srand(time(NULL));
     task1();
+    task2();
     return 0;
 }
